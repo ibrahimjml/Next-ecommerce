078af1e63052d75b184867507212cb062e11a7ac
ibrahimjml <anony.jjml@gmail.com>
2024-10-15 16:51:26 +0200

    edit cartpage and css

--- src/app/(pages)/cart/page.jsx
+++ src/app/(pages)/cart/page.jsx
@@ -1,6 +1,7 @@
 import "./cart.css"
 import Header from '@/app/components/Header/header'
 import Cartpage from "./cartpage"
+import Footer from "@/app/components/Footer/footer"
 
 
 export default function Cart() {
@@ -8,13 +9,12 @@ export default function Cart() {
 
   return (
     <>
-    <Header/>
-
-    <main style={{display:"flex",justifyContent:"center"}} >
+      
+        <div style={{height: "100vh",  display: "grid",  alignItems: "center",  gridTemplateRows: "auto 1fr auto",overflowY:"hidden"}}>
+            <Header/>
   <Cartpage/>
-</main>
-
-
+<Footer/>
+</div>
 </>
   )
 }
